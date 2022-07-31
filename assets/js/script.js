c5d3df27d9c3d0eba658f5490784bc17886d2e60
Emerson Okorie <emersonokorie@icloud.com>
2022-07-31 20:47:53 -0300

    Update 31/07

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
-alert ('Hello World')
+function load() {
+    var name = document.getElementById('name')
+    var text = document.getElementById('text')
+    var res = document.getElementById('res')
+    res.innerHTML= console.log(`${name}: ${text}`)
+}
+var nome = window.prompt('What is your name?')
+var sal= Number(window.prompt('Write your salary'))
+window.alert (`Welcome ${nome.toUpperCase()}, you earn ${sal.toLocaleString('pt-BR',{style: 'currency',currency:'BRL'})}`)
+
+
